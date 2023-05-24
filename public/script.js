92d69b1276f56ee545225c278f86edd73d6c41b6
Arafat <arafatshaikh823@gmail.com>
2023-05-25 04:07:50 +0530

    local storage features added

--- public/script.js
+++ public/script.js
@@ -5,29 +5,49 @@ const clearBtn = document.getElementById("clearAll-btn");
 const textInput = document.getElementById("text-input");
 const taskContainer = document.querySelector(".task");
 const taskText = document.querySelector(".task-text");
+const container = document.querySelector(".container");
 
-console.log(taskContainer);
+container.scrollTop = container.scrollHeight;
+let task = 1;
 
+textInput.addEventListener("keyup", (e) => {
 
-textInput.addEventListener("keyup",(e)=>{
-    if(e.key == "Enter"){
-        taskText.innerHTML = ""
-        let innerTask = document.createElement("div");
-        innerTask.className = "inner-task";
-        innerTask.innerHTML = `   
-                                <div class="task-name">
-                                <input type="checkbox" id="myCheckbox">
-                                <label for="myCheckbox">${textInput.value}</label>
-                                </div>
-                                <i class="fa-solid fa-ellipsis"></i>`
+    if (e.key == "Enter") {
+        task = localStorage.length;
+        console.log(task)
+        task += 1;
+        localStorage.setItem(task, textInput.value)
+        renderTask(textInput.value)
 
-
-    taskContainer.appendChild(innerTask);
-    textInput.value = "";
 
     }
 })
 
-clearBtn.addEventListener("click",()=>{
+function renderTask(task) {
+    taskText.innerHTML = "";
+    let innerTask = document.createElement("div");
+    innerTask.className = "inner-task";
+    innerTask.innerHTML = `   
+                            <div class="task-name">
+                            <input type="checkbox" id="myCheckbox">
+                            <label for="myCheckbox">${task}</label>
+                            </div>
+                            <i class="fa-solid fa-ellipsis"></i>`
+
+
+    taskContainer.appendChild(innerTask);
+    textInput.value = "";
+}
+
+clearBtn.addEventListener("click", () => {
     taskContainer.innerHTML = "";
+    localStorage.clear();
 })
+
+window.onload = ()=>{
+    for(let i=1; i<localStorage.length; i++){
+        let value = localStorage.getItem(i);
+        renderTask(value)
+    }
+}
+
